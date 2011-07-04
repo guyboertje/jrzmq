bcb735aa548f10e509af11a91e78c0df47ad1524
Guy Boertje <guyboertje@gmail.com>
2011-07-04 16:54:50 +0100

    finalised support for ruby and jruby use

--- lib/rbzmq.rb
+++ lib/rbzmq.rb
@@ -2,7 +2,7 @@ if RUBY_VERSION =~ /1\.8\.?/
   puts "You might have trouble using this library without proper threading support"
 end
 
-gem "zmq"
+require "zmq"
 
 require File.join("zmq","zmq")
 require File.join("common","queue")
